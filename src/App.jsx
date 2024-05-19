1687c4f273e314faf2dc9ba5a02333a8e5ba237d
Esther Santos <esthermendonca13@gmail.com>
2024-05-19 13:08:52 -0300

    criação dos primeiros estilos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { HomeContainer } from './pages/Home.styles';
+import './style.css'
+
+function App() {
+  return (
+    <HomeContainer>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </HomeContainer>
+  )
+}
+
+export default App
